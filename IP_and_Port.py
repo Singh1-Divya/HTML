1bd4851ceb56da5a21c71d20bd63aa967e8595a5
Singh1-Divya <divyasingh876708@gmail.com>
2024-08-18 13:34:13 +0530

    HTML basics

---
+++ IP_and_Port.py
@@ -0,0 +1,6 @@
+# IP address := Internet protocol(IP) address is the unique identifying number assigned to 
+# every device connected to the Internet.
+
+
+# Port address := Port adddress is the addressing information used to identify the sender 
+# and receivers of message in computer networking. 
